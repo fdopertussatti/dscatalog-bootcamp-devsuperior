113ddec50fed82e3992a86e1f8090388e84be6db
Fernando Pertussatti <fdopertussatti@gmail.com>
2021-01-14 16:39:24 -0500

    Database seeding

--- backend/src/main/java/com/pertusoftware/dscatalog/services/CategorysService.java
+++ backend/src/main/java/com/pertusoftware/dscatalog/services/CategorysService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.pertusoftware.dscatalog.entities.Category;
 import com.pertusoftware.dscatalog.repositories.CategoryRepository;
@@ -14,6 +15,7 @@ public class CategorysService {
 	@Autowired
 	private CategoryRepository reposotory;
 
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return reposotory.findAll();
 	}
