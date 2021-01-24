643f8c494979fce53dbca1102a0f7cf0c095dffc
Fernando Pertussatti <fdopertussatti@gmail.com>
2021-01-24 15:34:20 -0500

    Add Navbar

--- front-web/src/core/components/NavBar/index.tsx
+++ front-web/src/core/components/NavBar/index.tsx
@@ -1,24 +1,24 @@
- import React from 'react';
- import './styles.scss';
+import React from 'react';
+import './styles.scss';
 
-const NavBar = () =>(
+const NavBar = () => (
     <nav className="row bg-primary main-nav">
         <div className="col-2">
             <a href="link" className="nav-logo-text">
                 <h4>DS Catalog</h4>
             </a>
         </div>
-        <div className="col-6">
+        <div className="col-6 offset-2" >
             <ul className="main-menu">
                 <li>
-                    <a href="link">
+                    <a href="link" className="active">
                         HOME
                     </a>
                 </li>
                 <li>
                     <a href="link">
-                            CATALOG
-                    </a>             
+                        CATALOG
+                    </a>
                 </li>
                 <li>
                     <a href="link">
