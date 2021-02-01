8205b60b7bcdd54100f1c46306557a03215874f8
Fernando Pertussatti <fdopertussatti@gmail.com>
2021-01-31 21:50:24 -0500

    Add product linsting with backend

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 import ProductCard from './components/ProductCard';
 import './styles.scss';
-const   Catalog = () =>(
-    <div className="catalog-container">
-    <h1 className="catalog-title">
-        Catalogo de produtos
-    </h1>
-    <div className="catalog-products">
-        <Link to="/products/1"><ProductCard/></Link>
-        <Link to="/products/2"><ProductCard/></Link>
-        <Link to="/products/3"><ProductCard/></Link>
-        <Link to="/products/4"><ProductCard/></Link>
-        <Link to="/products/5"><ProductCard/></Link>
-        <Link to="/products/6"><ProductCard/></Link>
-        <Link to="/products/7"><ProductCard/></Link>
-        <Link to="/products/8"><ProductCard/></Link>
-        <Link to="/products/9"><ProductCard/></Link>
-    </div>
+import { makeRequest } from '../../core/utils/request';
+import { ProductsResponse } from '../../core/types/Product';
 
-    </div>
-);
+const   Catalog = () => {
+    //1 - quando o componente iniciar, buscar a lista de produtos
+    const[productsResponse, setProductResponse] = useState<ProductsResponse>();
+
+    //2 - quando a lista de produtos estiver disponível,
+    //popular um estado no comonente e listar os produtos dinamicamente.
+
+    console.log(productsResponse);
+
+    useEffect(() => {
+        const params = {
+            page: 0,
+            linesPerPage: 12 
+        }
+
+        makeRequest({url: '/products', params })
+        .then(response => setProductResponse(response.data));
+    }, []);
+
+    return (
+        <div className="catalog-container">
+            <h1 className="catalog-title">
+                Catalogo de produtos
+            </h1>
+            <div className="catalog-products">
+                {productsResponse?.content.map(product =>(
+                    <Link to={`/products/${product.id}`} key={product.id}>
+                        <ProductCard product={product}/>
+                    </Link>
+                ))}
+            </div>    
+        </div>     
+    );
+}
 
 export default Catalog;
