24b391b51f5daea7b67615272779cba87a9bb6b4
Fernando Pertussatti <fdopertussatti@gmail.com>
2021-02-17 21:15:24 -0500

    Basic beans validation

--- backend/src/main/java/com/pertusoftware/dscatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/pertusoftware/dscatalog/dto/ProductDTO.java
@@ -6,18 +6,32 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
 import com.pertusoftware.dscatalog.entities.Category;
 import com.pertusoftware.dscatalog.entities.Product;
 
 public class ProductDTO implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private Long id;
+
+	@Size(min = 5, max = 60, message = "Deve ter entre 5 e 60 caracteres")
+	@NotBlank(message = "Campo obrigatório")
 	private String name;
+
+	@NotBlank(message = "Campo requerido")
 	private String description;
+
+	@Positive(message = "O preço deve ser um valor positivo")
 	private Double price;
 	private String imgUrl;
+
+	@PastOrPresent(message = "A data do produto não pode ser futura")
 	private Instant date;
 
 	private List<CategoryDTO> categories = new ArrayList<>();
@@ -104,7 +118,5 @@ public class ProductDTO implements Serializable {
 	public void setCategories(List<CategoryDTO> categories) {
 		this.categories = categories;
 	}
-	
-	
 
 }
