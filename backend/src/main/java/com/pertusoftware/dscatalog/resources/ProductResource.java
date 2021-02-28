4b1a54d4db82885c0e7322fc367b45c4c20d828c
Fernando Pertussatti <fdopertussatti@gmail.com>
2021-02-28 16:05:46 -0500

    Upload endpoit

--- backend/src/main/java/com/pertusoftware/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/pertusoftware/dscatalog/resources/ProductResource.java
@@ -18,9 +18,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.pertusoftware.dscatalog.dto.ProductDTO;
+import com.pertusoftware.dscatalog.dto.UriDTO;
 import com.pertusoftware.dscatalog.services.ProductsService;
 
 @RestController
@@ -33,7 +35,7 @@ public class ProductResource {
 	// Lista todos os objetos
 	@GetMapping
 	public ResponseEntity<Page<ProductDTO>> findAll(
-			
+
 			@RequestParam(value = "categoryId", defaultValue = "0") Long categoryId,
 			@RequestParam(value = "name", defaultValue = "") String name,
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
@@ -74,6 +76,12 @@ public class ProductResource {
 		service.delete(id);
 
 		return ResponseEntity.noContent().build();
+	}
+
+	@PostMapping(value = "/image")
+	public ResponseEntity<UriDTO> uploadImage(@RequestParam("file") MultipartFile file) {
+		UriDTO dto = service.uploadFile(file);
+		return ResponseEntity.ok().body(dto);
 	}
 
 }
